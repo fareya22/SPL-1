d665b5a27a45a1a4d3df63be39f2734a97a7b0ab
fareya22 <113286085+fareya22@users.noreply.github.com>
2023-05-18 16:07:59 +0600

    inputfile

---
+++ sampleInput.cpp
@@ -0,0 +1,13 @@
+
+      int i=0;
+      int j =10;
+      if(i>2)
+      {
+            printf("Greater then 2");
+      }
+      else
+      {
+            k = j;
+      }
+      i =2;
+     printf("%d",i);
